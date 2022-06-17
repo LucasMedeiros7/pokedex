62e71f4c920cb54f7f52462eaf85d48c8740ff9c
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-06-17 13:04:14 -0300

    Refatorando JS

---
+++ js/functions.js
@@ -0,0 +1,59 @@
+//ARMAZENAMENTO DE DADOS DOS POKEMONS
+const pokemons = [];
+
+//MVC - CONTROLLER
+function getPokemon(nome) {
+  const request = new XMLHttpRequest();
+
+  request.open(
+    "GET",
+    `https://pokeapi.co/api/v2/pokemon/${nome.toLowerCase()}`
+  );
+  request.send();
+
+  request.addEventListener("load", () => {
+    if (request.status == 200) {
+      const pkm = JSON.parse(request.responseText);
+
+      createPokemon(
+        pkm.id,
+        pkm.name,
+        pkm.sprites.other.dream_world.front_default
+      );
+    } else {
+      p.innerText = "Este pokémon não existe";
+      p.style.color = "red";
+      modal.style.display = "flex";
+      input.value = "";
+    }
+  });
+}
+
+//MVC - MODEL
+function createPokemon(id, name, image) {
+  const pokemon = {
+    key: pokemons.length + 1,
+    id: id,
+    name: name,
+    image: image,
+  };
+  pokemons.push(pokemon);
+
+  render(pokemon);
+}
+
+//MVC - VIEW
+function render(pokemon) {
+  const nome = pokemon.name;
+  input.value = "";
+
+  div.innerHTML += `
+    <div id="${pokemon.key}" class="card">
+     <img src="${pokemon.image}" alt="">
+    <div class="info">
+       <span class="id-poke">#${pokemon.id}</span>
+      <span class="nome-poke">${
+        nome[0].toUpperCase() + nome.substring(1)
+      }</span>
+    </div>`;
+}
--- js/main.js
+++ js/main.js
@@ -1,127 +1,33 @@
 const div = document.getElementById("list");
 const btn = document.getElementById("botao");
-const buscar = document.getElementById("buscar");
-const modal = document.getElementById("div-modal")
-const p = document.getElementById("p")
-const animar = document.getElementById('modal')
+const input = document.getElementById("input");
+const modal = document.getElementById("div-modal");
+const p = document.getElementById("p");
 
-modal.onclick = () => {
-  modal.style.display = "none"
-}
-
+//refatorar essa parte
 btn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let valor = buscar.value.toLowerCase()
-  let idPoke = document.getElementById(valor)
+  let isNew;
 
-  if (idPoke) {
-    if (valor == idPoke.id) {
-      p.innerText = `O ${valor.toLowerCase()} já está na sua pokédex`
-      p.style.color = '#2A74BA'
-      modal.style.display = 'flex'
-      return
-    }
+  const nome = input.value === "" ? false : input.value;
+
+  if (nome) {
+    input.style.border = "initial";
+    isNew =
+      pokemons.filter((pokemon) => pokemon.name === nome.toLowerCase())
+        .length === 0;
   }
 
-  if (buscar.value != "") {
-    buscar.style.border = "";
+  if (isNew) getPokemon(nome);
+  else if (nome) {
+    p.innerText = `O ${nome[0].toUpperCase() + nome.substring(1).toLowerCase()} 
+    já está na sua pokédex`;
 
-    let pedido = new XMLHttpRequest();
-    pedido.open("GET", `https://pokeapi.co/api/v2/pokemon/${valor}`);
-    pedido.send();
-    pedido.addEventListener("load", () => {
-      if (pedido.status == 200) {
-        let pokemon = JSON.parse(pedido.responseText);
+    p.style.color = "#2A74BA";
+    modal.style.display = "flex";
+    input.value = "";
+  } else input.style.border = "red solid 2px";
+});
 
-        let nome = pokemon.name;
-        let foto = pokemon.sprites.other.dream_world.front_default;
-        let id = pokemon.id;
-        let altura = pokemon.height;
-        let tipo = pokemon.types[0].type.name;
-        let peso = pokemon.weight;
-        let habilidade = pokemon.abilities[0].ability.name;
-
-        criaCard(nome, foto, id, altura, tipo, peso, habilidade);
-      } else {
-        p.innerText = 'Este pokémon não existe'
-        p.style.color = 'red'
-        modal.style.display = 'flex'
-        buscar.value = '';
-      }
-    });
-  } else buscar.style.border = "2px solid #F11828";
-})
-
-//Pegando elementos do modal de detalhes
-
-
-
-function criaCard(nome, foto, id, altura, tipo, peso, habilidade) {
-
-  console.log('foifoifoif')
-
-  if (foto) {
-    div.innerHTML += `
-  <div onclick="criaCard('${nome}')" id="${nome.toLowerCase()}" class="card">
-  <img src="${foto}" alt="">
-  <div class="info">
-    <span class="id-poke">#${id}</span>
-    <span class="nome-poke">${nome.toUpperCase()}</span>
-  </div>
-   `;
-
-    modalPokeDetails.innerHTML += `
-   <div  class="modal-${nome} modal-details" >
-   <section id="pokemon-details" class="animar2">
-     <div class="pokemon-name">
-       <p id="name">${nome.toUpperCase()}</p>
-     </div>
-
-     <figure class="pokemon-figure">
-       <img class="pokemon-img" src="${foto}" alt="Imagem do Pokémon" />
-     </figure>
-
-     <div id="details">
-       <div class="height-details">
-         <h2>Altura</h2>
-         <p id="altura">${altura}</p>
-       </div>
-       <div class="type-details">
-         <h2>Tipo</h2>
-         <p id="tipo">${tipo}</p>
-       </div>
-       <div class="weight-details">
-         <h2>Peso</h2>
-         <p id="peso">${peso}</p>
-       </div>
-       <div class="hability-details">
-         <h2>Habilidade</h2>
-         <p id="habilidade">${habilidade}</p>
-       </div>
-     </div>
-   </section>
- </div> 
-   `
-    buscar.value = '';
-  }
-
-  else {
-    let cartinha = document.querySelector(`.modal-${nome}`)
-
-    modalPokeDetails.style.display = 'flex'
-    cartinha.style.display = 'flex';
-
-    return
-  }
-}
-
-const modalPokeDetails = document.getElementById('modal-poke-details')
-
-
-modalPokeDetails.addEventListener('click', () => {
-  modalPokeDetails.style.display = "none";
-
-})
-
-
+modal.onclick = () => (modal.style.display = "none");
